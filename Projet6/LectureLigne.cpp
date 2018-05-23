c1b4108eb19dd50e6100c23bbf04daa366b203a5
cgibert <cgibert@IF501-210-04>
2018-05-23 15:09:54 +0200

    Lecture ligne

--- Projet6/LectureLigne.cpp
+++ Projet6/LectureLigne.cpp
@@ -1,23 +1,21 @@
 /*******************************************************************************************
-Empreinte  -  description
+LectureLigne  -  description
 -------------------
 début                : 10/01/2018
-copyright            : (C) 2018 par
-e-mail               :
+copyright            : (C) 2018 par Christophe Gibert - Alexandra Verrier
+e-mail               : christophe.gibert@insa-lyon.fr - alexandra.verrier@insa-lyon.fr
 ********************************************************************************************/
 
-
-//---------- Réalisation de la classe <Empreinte> (fichier Empreinte.cpp) ------------
+//---------- Réalisation de la classe <LectureLigne> (fichier LectureLigne.cpp) ------------
 
 //---------------------------------------------------------------- INCLUDE
 
 //-------------------------------------------------------- Include système
-using namespace std;
 
+#include <iostream>
 
 //------------------------------------------------------ Include personnel
-#include "Empreinte.h"
-#include <fstream>
+#include "LectureLigne.h"
 
 //------------------------------------------------------------- Constantes
 
@@ -25,62 +23,172 @@ using namespace std;
 
 //----------------------------------------------------- Méthodes publiques
 
-int Empreinte::getID() {
-	return ID;
+string LectureLigne::GetID() const
+{
+	return source;
+} //----- Fin de GetSource
+
+string LectureLigne::GetDestination() const
+{
+	return destination;
+}//----- Fin de GetDestination
+
+int LectureLigne::GetHeure() const
+{
+	return heure;
+} //----- Fin de GetHeure
+
+string LectureLigne::ExtractionExtension(const string& page)
+// Algorithme : On place un itérateur inversé sur le string passé en paramètre
+//		et on récupère tous les caractères, en appelant extractionPage, jusqu'à rencontrer un point.
+{
+	string::const_reverse_iterator it = page.rbegin();
+	string extension = extractionPage(it, '.');
+	return extension;
+} //----- Fin de ExtractionExtension
+
+  //------------------------------------------------- Surcharge d'opérateurs
+
+  //-------------------------------------------- Constructeurs - destructeur
+LectureLigne::LectureLigne(const string& ligneLog)
+// Algorithme : On coupe la ligne avec l'appel de splitter et on rempli les 
+//		attributs en évocant éventuellement des méthodes.
+{
+	vector<string> ligne(spliter(ligneLog));
+	adresseIP = ligne[0];
+	date = ligne[3];
+	requete = ligne[4];
+	status = atoi(ligne[5].c_str());
+	tailleOctet = atoi(ligne[6].c_str());
+	adresseSource = ligne[7];
+	idNavigateur = ligne[8];
+	heure = extractionHeure(date);
+	destination = extractionDestination(requete);
+	source = extractionSource(adresseSource);
 }
 
-Maladie* Empreinte::getMaladie() {
-	return maMaladie;
-}
 
-void Empreinte::remplirVecteurs(string mesAttributs, string maMaladieAttributs) {
-
-}
-
-//------------------------------------------------- Surcharge d'opérateurs
-
-//-------------------------------------------- Constructeurs - destructeur
-
-Empreinte::Empreinte()
-// Algorithme : 
+LectureLigne::~LectureLigne()
 {
 #ifdef MAP
-	cout << "Appel au constructeur de <Empreinte>" << endl;
+	cout << "Appel au destructeur de <LectureLigne>" << endl;
 #endif
-} //----- Fin de Empreinte
+} //----- Fin de ~LectureLigne
 
-Empreinte::Empreinte(int id, Maladie * uneMaladie)
-// Algorithme : 
-{
-#ifdef MAP
-	cout << "Appel au constructeur de <Empreinte>" << endl;
-#endif
-	ID = id;
-	maMaladie = uneMaladie;
-} //----- Fin de Empreinte
 
-Empreinte::~Empreinte()
-{
-#ifdef MAP
-	cout << "Appel au destructeur de <Empreinte>" << endl;
-#endif
-} //----- Fin de ~Empreinte
+  //------------------------------------------------------------------ PRIVE
 
-void Empreinte::remplirEmpreinte(string nomFichier)
+  //----------------------------------------------------- Méthodes protégées
+vector<string> LectureLigne::spliter(const string& ligne)
+// Algorithme : On coupe la ligne donnée en paramètre avec comme caractère de séparation l'espace.
+//		Si on est entre des crochets on ignore l'espace.
 {
-	ifstream infile(nomFichier.c_str());
-	if (infile)
+	vector<string> tableau(9, "");
+	unsigned int compteur = 0;
+	bool espace = true; // permet de savoir si on prend l'espace comme séparateur ou si on l'ignore
+	string::const_iterator it;
+	for (it = ligne.begin(); it != ligne.end(); ++it)
 	{
-		string ligne;
-		while (getline(infile, ligne))
+		if (*it == '[')
 		{
-
+			espace = false;
+		}
+		else if (*it == ']')
+		{
+			espace = true;
+		}
+		else if (*it == '"')
+		{
+			espace = !espace;
+		}
+		else if ((espace) && (*it == ' '))
+		{
+			compteur++;
+			if (compteur == tableau.size())
+			{
+				tableau.push_back("");
+			}
+		}
+		else
+		{
+			tableau[compteur] += *it;
 		}
 	}
-
+	return tableau;
 }
 
+int LectureLigne::extractionHeure(const string& date)
+// Algorithme : On place un itérateur sur la date et on en extrait seulement l'heure 
+//		(les 2 caractères suivants le premier ":").
+//		On convertit les caractères obtenus en entiers.
+{
+	string::const_iterator it = date.begin();
+	while (*it != ':')
+	{
+		++it;
+	}
+	string sHeure = "";
+	sHeure += *(++it);
+	++it;
+	sHeure += *it;
 
-//------------------------------------------------------------------ PRIVE
+	int iHeure = atoi(sHeure.c_str());
 
-//----------------------------------------------------- Méthodes protégées
+	return iHeure;
+}
+
+string LectureLigne::extractionPage(string::const_reverse_iterator it, const char separateur)
+// Algorithme : On utilise un itérateur inversé sur un string passé en paramètre
+//		pour récupérer tous les caractères jusqu'à rencontrer du caractère séparateur.
+//		On inverse la chaîne de caractère obtenue pour qu'elle corresponde à la bonne extraction.
+{
+	string page = "";
+	//remplissage de la destination
+	while (*it != separateur)
+	{
+		page += *it;
+		++it;
+	}
+	//Inversion du string
+	int n = page.length();
+	for (int i = 0; i<n / 2; i++)
+	{
+		swap(page[i], page[n - i - 1]);
+	}
+	return page;
+}
+
+string LectureLigne::extractionDestination(const string& requete)
+// Algorithme : On place correctement un itérateur inversé sur le string passé en paramètre
+//				et on récupère tous les caractères, en appelant extractionPage, jusqu'à rencontrer un espace.
+{
+	string::const_reverse_iterator it = requete.rbegin();
+	while (*it != ' ')
+	{
+		++it;
+	}
+	++it;
+
+	string destination = extractionPage(it, ' ');
+	return destination;
+}
+
+string LectureLigne::extractionSource(const string& adresseSource)
+// Algorithme : On teste si l'adresse de la source contient la base de l'url local.
+//				Si c'est le cas on le supprime.
+{
+	string source = adresseSource;
+	string ref = "http://intranet-if.insa-lyon.fr";
+	string sourceFinale = "";
+	if (source.compare(0, ref.length(), ref) == 0) {
+		for (unsigned int i = ref.length(); i< source.length(); ++i)
+		{
+			sourceFinale += source[i];
+		}
+	}
+	else
+	{
+		sourceFinale = source;
+	}
+	return sourceFinale;
+}
--- Projet6/LectureLigne.h
+++ Projet6/LectureLigne.h
@@ -1,67 +1,118 @@
-/************************************************************************************
-LECTURELIGNE  -  description
+/*******************************************************************************************
+LectureLigne  -  description
 -------------------
 début                : 10/01/2018
-copyright            : (C) 2018 par
-e-mail               :
-*************************************************************************************/
+copyright            : (C) 2018 par Christophe Gibert -
+e-mail               : christophe.gibert@insa-lyon.fr
+********************************************************************************************/
 
-//---------- Interface de la classe <Empreinte> (fichier Empreinte.h) ----------------
+//---------- Interface de la classe <LectureLigne> (fichier LectureLigne.h) ----------------
 #if ! defined ( LECTURELIGNE_H )
 #define LECTURELIGNE_H
-
-//--------------------------------------------------- Interfaces utilisées
-#include <string>
+using namespace std;
 #include <vector>
+#include <string>
+//--------------------------------------------------- Interfaces utilisées
+
 //------------------------------------------------------------- Constantes
 
 //------------------------------------------------------------------ Types
 
 //------------------------------------------------------------------------
-// Rôle de la classe <Empreinte>
+// Rôle de la classe <LectureLigne>
+// Cette classe permet d'analyser une ligne d'un fichier log et d'en extraire 
+// les informations utiles.
 //------------------------------------------------------------------------
 
-class Empreinte
+class LectureLigne
 {
 	//----------------------------------------------------------------- PUBLIC
 
 public:
 	//----------------------------------------------------- Méthodes publiques
+	string GetID() const;
+	// Mode d'emploi : Renvoie le referer de la ligne lue dans le fichier .log.
+
+	string GetDestination() const;
+	// Mode d'emploi : Renvoie la requête de la ligne lue dans le fichier .log.
+
+	int GetHeure() const;
+	// Mode d'emploi : Renvoie l'heure de la requête de la ligne lue dans le fichier .log.
+
+	string ExtractionExtension(const string& page);
+	// Mode d'emploi : Renvoie l'extension du referer ou de la destination de la requête.
 
 	//------------------------------------------------- Surcharge d'opérateurs
-	Empreinte & operator = (const Empreinte & unEmpreinte) = delete;
+	LectureLigne & operator = (const LectureLigne & unLectureLigne) = delete;
 	// Mode d'emploi :
-	// Il s'agit de la surcharge de l'opérateur = pour des objets Empreinte.
+	// Surcharge de l'opérateur = pour les objets LectureLigne.
 	// Contrat :
-	// déclaré mais non défini pour intercepter son éventuel appel.
+	// déclaration mais pas de définition pour intercepter son utilisation éventuelle.
 
 	//-------------------------------------------- Constructeurs - destructeur
-	Empreinte(const Empreinte & unEmpreinte) = delete;
+	LectureLigne(const LectureLigne & unLectureLigne) = delete;
 	// Mode d'emploi :
-	// Il s'agit du constructueur de copie du objet Empreinte.
+	// Constructeur de copie
 	// Contrat :
-	// déclaré mais non défini pour intercepter son éventuel appel.
+	// déclaration mais pas de définition pour intercepter son utilisation éventuelle
 
-
-	Empreinte();
+	LectureLigne();
 	// Mode d'emploi :
-	// Il s'agit du constructueur (sans paramètre) par défaut des objets Empreinte.
+	// Constructeur par défaut, sans paramètre
 
-	virtual ~Empreinte();
+	LectureLigne(const string& ligneLog);
 	// Mode d'emploi :
-	// Il s'agit du destructeur des objets Empreinte.
+	// Constructeur d'objet LectureLigne avec une ligne du fichier log en paramètre
+
+	virtual ~LectureLigne();
+	// Mode d'emploi :
+	// Destructeur d'objet LectureLigne
 
 	//------------------------------------------------------------------ PRIVE
 
 protected:
 	//----------------------------------------------------- Méthodes protégées
+	int extractionHeure(const string& date);
+	// Mode d'emploi :
+	// Permet d'extraire seulement l'entier correspondant à l'heure à partir de la date.
 
+	string extractionDestination(const string& requete);
+	// Mode d'emploi :
+	// Permet d'extraire seulement la destination correspondant une requête. 
+
+	string extractionSource(const string& adresseSource);
+	// Mode d'emploi :
+	// Permet d'extraire seulement la source correspondant une adresse.
+	// Contrat :
+	// Si la source est locale alors la base de l'url est retirée.
+
+	vector<string> spliter(const string& ligne);
+	// Mode d'emploi :
+	// Coupe la ligne de code du fichier log pour séparer les différentes parties qui sont stockées dans un vecteur.
+
+	string extractionPage(string::const_reverse_iterator it, const char separateur);
+	// Mode d'emploi :
+	// Permet d'extraire une partie d'un string associé à un itérateur.
+	// Contrat :
+	// Le caractère permettant de définir ce que l'on veut extraire doit être spécifié en paramètre.
 
 	//----------------------------------------------------- Attributs protégés
+	int ID;
 
+
+	string adresseSource;
+	string source;
+	string destination;
+	string date;
+	string adresseIP;
+	string requete;
+	int status;
+	int tailleOctet;
+	string idNavigateur;
+	int heure;
 
 };
 
-//-------------------------------- Autres définitions dépendantes de <Empreinte>
+//-------------------------------- Autres définitions dépendantes de <LectureLigne>
 
-#endif // LECTURELIGNE_H#pragma once
+#endif // LECTURELIGNE_H

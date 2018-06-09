b052799280a7e43abaa7c9fd683ced6743e8bccc
ferys <ferys@DESKTOP-J59G5HP>
2018-06-09 16:24:28 +0200

    ye

--- Projet6/TestPerformance.h
+++ Projet6/TestPerformance.h
@@ -59,7 +59,7 @@ protected:
 
     //----------------------------------------------------- Attributs protégés
 
-    const string folder = "TestPerformance/Test";
+    const string folder = "TestPerformance/TestDico";
 
     const string dictionnaire = "/Dictionnaire1.txt";
     const string mesures = "/HealthMesure1.txt";
